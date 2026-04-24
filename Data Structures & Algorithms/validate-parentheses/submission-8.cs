d63d1f97e06049d8d5b77916dbcc44a2ddb10da1
qalvik <46355180+qalvik@users.noreply.github.com>
2026-04-24 17:12:29 +0200

    Add: validate-parentheses - submission-8

---
+++ Data Structures & Algorithms/validate-parentheses/submission-8.cs
@@ -0,0 +1,40 @@
+public class Solution {
+    public bool IsValid(string s) {
+        var stack = new Stack<char>();
+        
+        if (s.Length % 2 != 0)
+            return false;
+
+        var result = false;
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+
+            if (c == '(' || c == '[' || c == '{')
+                stack.Push(c);
+            else 
+            {   
+                if (stack.Count == 0)
+                    return false;
+
+                result = true;
+                var element = stack.Pop();
+
+                if (element == '(' && c != ')')
+                    return false;
+
+                if (element == '[' && c != ']')
+                    return false;
+
+                if (element == '{' && c != '}')
+                    return false;
+            }
+        }
+
+        if (stack.Count != 0)
+            return false;
+            
+        return result;
+    }
+}
