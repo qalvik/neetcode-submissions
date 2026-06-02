ab8085c6b03716bdad51c7e490667ffae914bf47
qalvik <46355180+qalvik@users.noreply.github.com>
2026-06-02 16:03:04 +0200

    Add: binary-search - submission-5

---
+++ Data Structures & Algorithms/binary-search/submission-5.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+       return BinarySearch(nums, target, 0, nums.Length - 1);
+    }
+
+    private int BinarySearch(int[] nums, int target, int l, int r)
+    {
+        if (l == r || l < 0 || r < 0)
+            return target == nums[l] ? l : -1;
+        
+        var m = (l + r) / 2;
+    
+        if (nums[m] == target)
+            return m;
+        if (nums[m] > target)
+            return BinarySearch(nums, target, l, m - 1);
+        else 
+            return BinarySearch(nums, target, m + 1 , r);
+    }
+}
